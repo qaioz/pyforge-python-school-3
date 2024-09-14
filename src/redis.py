92d2497111061dbb6dc7aec9cb471d5e027cdc8c
Gaioz <tabtadzegaga@gmail.com>
2024-09-14 17:24:48 +0400

    upgrade caching servce

--- src/redis.py
+++ src/redis.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Type
 
 import redis
 import logging
@@ -23,6 +23,39 @@ class RedisCacheService:
     def get_json(self, key) -> dict:
         return self.redis_client.json().get(key, ".")
 
+    def cached(self, key_prefix: str = None, key_args: list[str] = None):
+        def wrapper(func):
+            def wrapped(*args, **kwargs):
+                if args:
+                    raise ValueError("Cached function must not have positional arguments")
+
+                key = self.generate_caching_key(key_prefix, key_args, **kwargs)
+                cached = self.get_json(key)
+                if cached:
+                    return cached
+                result = func(*args, **kwargs)
+                self.set_json(key, result)
+                return result
+
+            return wrapped
+
+        return wrapper
+
+    @staticmethod
+    def generate_caching_key(key_prefix: str, key_args: list[str], **kwargs) -> str:
+        # filter out None values and those that are not in key_args also, it is important to sort the kwargs
+        # according to argument name to make sure that the key is always the same, regardless of the order
+        filtered_sorted_kwargs_tuples = sorted([
+            (k, v) for k, v in kwargs.items() if v is not None and (not key_args or k in key_args)
+        ])
+
+        key_builder = [key_prefix]
+
+        for k, v in filtered_sorted_kwargs_tuples:
+            key_builder.append(f"{k}={v}")
+
+        return ":".join(key_builder)
+
 
 def get_redis_credentials(settings: Annotated[Settings, Depends(get_settings)]) -> dict:
     return dict(
@@ -33,9 +66,8 @@ def get_redis_credentials(settings: Annotated[Settings, Depends(get_settings)]) 
 
 
 def get_redis_client(
-    redis_credentials: Annotated[dict, Depends(get_redis_credentials)]
+        redis_credentials: Annotated[dict, Depends(get_redis_credentials)]
 ) -> redis.Redis:
-
     client = redis.Redis(
         host=redis_credentials["host"], port=redis_credentials["port"], db=0
     )
@@ -46,6 +78,6 @@ def get_redis_client(
 
 @lru_cache
 def get_redis_cache_service(
-    redis_client: Annotated[redis.Redis, Depends(get_redis_client)]
+        redis_client: Annotated[redis.Redis, Depends(get_redis_client)]
 ) -> RedisCacheService:
     return RedisCacheService(redis_client)
